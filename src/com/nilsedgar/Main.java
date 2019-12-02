4de56b3a0d2f3cdd293ef2a0d16af9f1c0ae893d
Nils Jacobsen <nilsedgarjacobsen@gmail.com>
2019-12-02 13:38:34 +0100

    created object in main

--- src/com/nilsedgar/Main.java
+++ src/com/nilsedgar/Main.java
@@ -3,6 +3,9 @@ package com.nilsedgar;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+        FootballSimulator footballSimulator = new FootballSimulator();
+        footballSimulator.showMainMenu();
+
     }
 }
